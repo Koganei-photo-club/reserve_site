32437c0bd5c917e814b486f33eba02f1242b99e3
Fujiyoshi Takayuki <takayuki.fujiyoshi.2r@stu.hosei.ac.jp>
2025-12-01 21:31:25 +0900

    set plain content

--- mypage.js
+++ mypage.js
@@ -18,14 +18,14 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   const user = JSON.parse(userJson);
 
-  const gradeNames = ["","B1","B2","B3","B4","M1","M2"];
-  const roleNames  = ["役職なし","部長","副部長","会計","文連"];
+  // const gradeNames = ["","B1","B2","B3","B4","M1","M2"];
+  // const roleNames  = ["役職なし","部長","副部長","会計","文連"];
 
   document.getElementById("mp-name").textContent  = user.name;
-  document.getElementById("mp-grade").textContent = gradeNames[user.grade] ?? "ー";
+  document.getElementById("mp-grade").textContent = user.grade ?? "ー";
   document.getElementById("mp-line").textContent  = user.lineName;
   document.getElementById("mp-email").textContent = user.email;
-  document.getElementById("mp-role").textContent  = roleNames[user.role] ?? "ー";
+  document.getElementById("mp-role").textContent  = user.role ?? "ー";
 
   document.getElementById("logoutBtn").onclick = () => {
     sessionStorage.clear();
